e32b6ef7a641212d43e7b0d526d403a58e963048
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2023-12-19 10:57:57 +0800

    N1test
    N1test upload to Github

---
+++ Alphav01/src/main/java/com/mycompany/alphav01/Alphav01.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alphav01;
+import java.util.*;
+/**
+ *
+ * @author student
+ */
+public class Alphav01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        String s ;
+        Scanner scanner = new Scanner (System.in);
+        s=scanner.nextLine();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
